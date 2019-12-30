37768511ac87076080e7522e57a2c2bb005c8395
Lawrence <jijo.lawrence@accenture.com>
2019-12-30 01:24:11 -0500

    open api skelton source code regenerated

--- src/main/java/io/swagger/api/ApiException.java
+++ src/main/java/io/swagger/api/ApiException.java
@@ -1,6 +1,6 @@
 package io.swagger.api;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-11-22T19:39:28.611Z[GMT]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-12-30T05:54:51.453Z[GMT]")
 public class ApiException extends Exception{
     private int code;
     public ApiException (int code, String msg) {
--- src/main/java/io/swagger/api/ApiOriginFilter.java
+++ src/main/java/io/swagger/api/ApiOriginFilter.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-11-22T19:39:28.611Z[GMT]")
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2019-12-30T05:54:51.453Z[GMT]")
 public class ApiOriginFilter implements javax.servlet.Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response,
